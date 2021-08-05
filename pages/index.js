721f283cd9e1697d4727210e62c356bcb06bbb93
EN <iwagner76@gmail.com>
2021-08-05 16:52:58 -0400

    styling

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,11 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
-          {cardsData?.map((item) => (
-            <MediumCard />
-          ))}
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
