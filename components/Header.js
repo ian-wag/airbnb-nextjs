609d81bff24570b236856faf1e06e853e506b002
EN <iwagner76@gmail.com>
2021-08-05 14:38:19 -0400

    added logo and search bar to header

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 ">
+      {/* left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      {/* middle - Search*/}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex md:mx-2" />
+      </div>
+      {/* right */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
