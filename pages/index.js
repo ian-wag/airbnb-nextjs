1be18c3f37724917cdcfaf9405669acf74e37ec2
EN <iwagner76@gmail.com>
2021-08-05 16:23:35 -0400

    import SmallCard component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
 export default function Home({ exploreData }) {
   return (
@@ -18,8 +19,13 @@ export default function Home({ exploreData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           {/* pull data from server - API endpoints */}
-          {exploreData?.map((item) => (
-            <h1>{item.location}</h1>
+          {exploreData?.map(({ img, distance, location }) => (
+            <SmallCard
+              key={img}
+              img={img}
+              distance={distance}
+              location={location}
+            />
           ))}
         </section>
       </main>
