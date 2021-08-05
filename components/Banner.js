d128a1d086840a05c9e4ef64d41bc87b14b6203f
EN <iwagner76@gmail.com>
2021-08-05 15:17:51 -0400

    added hero image, text, and button

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,22 @@
 import Image from "next/image";
 
 function Banner() {
-  return <div></div>;
+  return (
+    <div className="relative h-[18.75rem] sm:h-[25rem] lg:h-[31.25rem] xl:h-[37.5rem] 2xl:h-[43.75]">
+      <Image
+        src="https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
