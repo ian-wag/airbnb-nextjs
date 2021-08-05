1a882ae29a7c5b9ce53fb97a4ebd8beba71335dc
EN <iwagner76@gmail.com>
2021-08-05 16:00:43 -0400

    added api fetch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -12,6 +12,29 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:p-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* pull data from server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://jsonkeeper.com/b/4G1G").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
