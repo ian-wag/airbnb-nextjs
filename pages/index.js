8343c8c81456a6daa7923540f3de94fa6f6a789a
EN <iwagner76@gmail.com>
2021-08-07 11:58:53 -0400

    added meta tags to head

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Airbnb Clone | By Me</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
