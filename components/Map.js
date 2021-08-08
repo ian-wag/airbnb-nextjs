7a681b6e420718d05bef0405adab0fde5e3d03d2
EN <iwagner76@gmail.com>
2021-08-08 01:26:04 -0400

    added mapbox integration

---
+++ components/Map.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+import { LocationMarkerIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/ianwag/cks2o26mb1cbu18qc49dxgvl6"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <LocationMarkerIcon
+              role="img"
+              className="h-10 text-red-400 cursor-pointer animate-bounce"
+              onClick={() => setSelectedLocation(result)}
+              aria-label="push-pin"
+            />
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+              className="z-50"
+            >
+              <div className=" relative p-12 bg-black">
+                <Image
+                  src={result.img}
+                  layout="fill"
+                  objectFit="cover"
+                  className="pb-1"
+                />
+              </div>
+              <p className="font-semibold text-lg">{result.title}</p>
+              <p className="text-sm">{result.total}</p>
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["upload.wikimedia.org", "a0.muscache.com", "links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiaWFud2FnIiwiYSI6ImNrczJuc2d3MzA5dXEycW5qemw2MnRsN24ifQ.o7tsnlpAlCCzKpOjx5PkUg",
+  },
 };
